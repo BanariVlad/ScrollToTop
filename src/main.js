aab4568965c40db9f0b34b560bec9f2af82feb15
BanariVlad <banari.vlad@yandex.com>
2020-06-19 19:22:22 +0300

    In directive

--- src/main.js
+++ src/main.js
@@ -4,13 +4,41 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
-Vue.directive("back", {
-  bind: function (el, binding) {
-    el.style.position = 'fixed'
-    el.style.top = binding.value + '%'
-    el.style.left = binding.value + '%'
+Vue.directive("back-to-top", {
+  bind(el) {
+    let visible = false;
+    el.style.display = "none";
+    el.style.fontSize = "35px";
+    el.innerHTML = "🠕";
+    el.style.top = "93%";
+    el.style.left = "93%";
+    el.style.width = "50px";
+    el.style.height = "50px";
+    el.style.position = "fixed";
+    el.style.cursor = "pointer";
+    el.style.borderRadius = "100%";
+    el.style.color = "#fff";
+    el.style.background = "#000";
+
+    const scrollBack = () => {
+      let scroll = document.documentElement.scrollTop;
+      if (scroll > 0) {
+        scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+      }
+    };
+
+    const followScroll = () => {
+      visible = window.pageYOffset > 250;
+      visible ? (el.style.display = "block") : (el.style.display = "none");
+    };
+
+    document.addEventListener("scroll", followScroll);
+    el.addEventListener("click", scrollBack);
   }
-})
+});
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <div class="back-to-top" v-back="96" v-show="visible" @click="backToTop">Back</div>
+    <div v-back-to-top></div>
     <p v-for="text in 25" :key="text">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus
       eleifend ante, et vehicula anteposuere id. Integer fermentum ultrices dui
-      vitae accumsan. Quisque metus eros, accumsan quis nisl vel,
-      molestie imperdiet lorem. Vivamus pulvinar id leo a suscipit. Vivamus rhoncus dui ac arcu placerat, aliquam
-      auctor nisl rhoncus. Cras bibendum molestie consectetur. Praesent pulvinar, eros ut maximus vulputate, nunc
-      magna bibendum mi, ac aliquam sem velit ac dui. Sed lorem odio, lobortis id molestie pretium, pretium et
-      nunc.
+      vitae accumsan. Quisque metus eros, accumsan quis nisl vel, molestie
+      imperdiet lorem. Vivamus pulvinar id leo a suscipit. Vivamus rhoncus dui
+      ac arcu placerat, aliquam auctor nisl rhoncus. Cras bibendum molestie
+      consectetur. Praesent pulvinar, eros ut maximus vulputate, nunc magna
+      bibendum mi, ac aliquam sem velit ac dui. Sed lorem odio, lobortis id
+      molestie pretium, pretium et nunc.
     </p>
   </div>
 </template>
@@ -18,44 +19,8 @@
 // @ is an alias to /src
 
 export default {
-  name: "Home",
-  data: () => ({
-    visible: false,
-    scrollBack: ""
-  }),
-
-  mounted() {
-    this.scrollBack = () => {
-      let scroll = document.documentElement.scrollTop;
-      if (scroll > 0) {
-        scrollTo({
-          top: 0,
-          behavior: "smooth"
-        });
-      }
-    };
-
-    document.addEventListener("scroll", this.followScroll);
-  },
-
-  methods: {
-    followScroll() {
-      this.visible = window.pageYOffset > 250;
-    },
-
-    backToTop() {
-      this.scrollBack();
-    }
-  }
+  name: "Home"
 };
 </script>
 
-<style scoped>
-.back-to-top {
-  position: fixed;
-  cursor: pointer;
-  background: #000;
-  color: #fff;
-  height: 100px;
-}
-</style>
+<style scoped></style>
